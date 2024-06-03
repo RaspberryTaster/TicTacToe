8f3494d919a4f55f15d9d00fdd365a165689de47
Anon <81258695+RaspberryTaster@users.noreply.github.com>
2024-06-03 23:57:16 +0100

    removed dead code

--- Scenes/computer_input/ComputerInput.gd
+++ Scenes/computer_input/ComputerInput.gd
@@ -8,19 +8,6 @@ func _on_player_on_make_move(_board, _marker):
 	$"../../CanvasLayer/VBoxContainer/TurnLabel".text = "Red turn"
 	var state = $"../..".get_current_game_state()
 	var availableChoices = $"../..".available_choices_in_game_state(state)
-	#var bestScore := -INF
-	#var bestChoice = null
-	#for choice in availableChoices:
-		#state[choice.x][choice.y] = computerPlayer
-		##HAS TO BE OPPOSITE OF WHAT IT IS CURRENTLY OR IT MESSES EVERYTHING UP
-		##var minimaxValue = minimax(state,3,false)
-		#var minimaxValue = 1
-		#state[choice.x][choice.y] = null
-		#print(choice,minimaxValue)
-		#if(minimaxValue > bestScore):
-			#
-			#bestScore = minimaxValue
-			#bestChoice = choice
 	var bestChoice = bestMove(state)
 	if(bestChoice != null):
 		$"../..".board[bestChoice.x][bestChoice.y]._on_button_pressed()
